3c0b521e814e6453b20ae23760e13d40ba865d14
Amina <yusufovamina@gmail.com>
2023-10-08 13:07:00 +0400

    Commited

--- personsList/Form1.Designer.cs
+++ personsList/Form1.Designer.cs
@@ -64,21 +64,19 @@
             // 
             listBox1.BackColor = Color.WhiteSmoke;
             listBox1.FormattingEnabled = true;
-            listBox1.ItemHeight = 15;
-            listBox1.Location = new Point(260, 137);
-            listBox1.Margin = new Padding(3, 2, 3, 2);
+            listBox1.ItemHeight = 20;
+            listBox1.Location = new Point(297, 183);
             listBox1.Name = "listBox1";
-            listBox1.Size = new Size(463, 214);
+            listBox1.Size = new Size(529, 284);
             listBox1.TabIndex = 0;
             // 
             // button1
             // 
             button1.BackColor = Color.Chartreuse;
             button1.Cursor = Cursors.Hand;
-            button1.Location = new Point(620, 58);
-            button1.Margin = new Padding(3, 2, 3, 2);
+            button1.Location = new Point(709, 77);
             button1.Name = "button1";
-            button1.Size = new Size(102, 29);
+            button1.Size = new Size(117, 39);
             button1.TabIndex = 1;
             button1.Text = "Add person";
             button1.UseVisualStyleBackColor = false;
@@ -88,10 +86,9 @@
             // 
             deleteButton.BackColor = Color.IndianRed;
             deleteButton.Cursor = Cursors.Hand;
-            deleteButton.Location = new Point(620, 372);
-            deleteButton.Margin = new Padding(3, 2, 3, 2);
+            deleteButton.Location = new Point(709, 496);
             deleteButton.Name = "deleteButton";
-            deleteButton.Size = new Size(97, 30);
+            deleteButton.Size = new Size(111, 40);
             deleteButton.TabIndex = 2;
             deleteButton.Text = "Delete person";
             deleteButton.UseVisualStyleBackColor = false;
@@ -99,26 +96,23 @@
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(89, 9);
-            textBox1.Margin = new Padding(3, 2, 3, 2);
+            textBox1.Location = new Point(102, 12);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(125, 23);
+            textBox1.Size = new Size(142, 27);
             textBox1.TabIndex = 3;
             // 
             // textBox2
             // 
-            textBox2.Location = new Point(89, 34);
-            textBox2.Margin = new Padding(3, 2, 3, 2);
+            textBox2.Location = new Point(102, 45);
             textBox2.Name = "textBox2";
-            textBox2.Size = new Size(125, 23);
+            textBox2.Size = new Size(142, 27);
             textBox2.TabIndex = 4;
             // 
             // textBox3
             // 
-            textBox3.Location = new Point(89, 58);
-            textBox3.Margin = new Padding(3, 2, 3, 2);
+            textBox3.Location = new Point(102, 77);
             textBox3.Name = "textBox3";
-            textBox3.Size = new Size(53, 23);
+            textBox3.Size = new Size(60, 27);
             textBox3.TabIndex = 5;
             // 
             // label1
@@ -126,9 +120,9 @@
             label1.AutoSize = true;
             label1.BackColor = Color.FromArgb(192, 192, 255);
             label1.Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            label1.Location = new Point(10, 12);
+            label1.Location = new Point(11, 16);
             label1.Name = "label1";
-            label1.Size = new Size(49, 19);
+            label1.Size = new Size(62, 22);
             label1.TabIndex = 7;
             label1.Text = "Name:";
             // 
@@ -137,9 +131,9 @@
             label2.AutoSize = true;
             label2.BackColor = Color.FromArgb(192, 192, 255);
             label2.Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            label2.Location = new Point(10, 35);
+            label2.Location = new Point(11, 47);
             label2.Name = "label2";
-            label2.Size = new Size(65, 19);
+            label2.Size = new Size(84, 22);
             label2.TabIndex = 8;
             label2.Text = "Surname:";
             // 
@@ -148,9 +142,9 @@
             label3.AutoSize = true;
             label3.BackColor = Color.FromArgb(192, 192, 255);
             label3.Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            label3.Location = new Point(10, 58);
+            label3.Location = new Point(11, 77);
             label3.Name = "label3";
-            label3.Size = new Size(37, 19);
+            label3.Size = new Size(48, 22);
             label3.TabIndex = 9;
             label3.Text = "Age:";
             // 
@@ -159,9 +153,9 @@
             label4.AutoSize = true;
             label4.BackColor = Color.FromArgb(192, 192, 255);
             label4.Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            label4.Location = new Point(10, 84);
+            label4.Location = new Point(11, 112);
             label4.Name = "label4";
-            label4.Size = new Size(54, 19);
+            label4.Size = new Size(68, 22);
             label4.TabIndex = 10;
             label4.Text = "Gender";
             // 
@@ -170,26 +164,27 @@
             Gender.Cursor = Cursors.Hand;
             Gender.FormattingEnabled = true;
             Gender.Items.AddRange(new object[] { "Male", "Female", "Other." });
-            Gender.Location = new Point(89, 82);
-            Gender.Margin = new Padding(3, 2, 3, 2);
+            Gender.Location = new Point(102, 109);
             Gender.Name = "Gender";
-            Gender.Size = new Size(133, 23);
+            Gender.Size = new Size(151, 28);
             Gender.TabIndex = 13;
             Gender.Text = "Select";
             Gender.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
             // 
             // monthCalendar1
             // 
-            monthCalendar1.Location = new Point(10, 165);
+            monthCalendar1.Location = new Point(11, 220);
+            monthCalendar1.Margin = new Padding(10, 12, 10, 12);
             monthCalendar1.Name = "monthCalendar1";
             monthCalendar1.TabIndex = 14;
             monthCalendar1.DateChanged += monthCalendar1_DateChanged_1;
             // 
             // ShowInfoButton
             // 
-            ShowInfoButton.Location = new Point(22, 381);
+            ShowInfoButton.Location = new Point(25, 508);
+            ShowInfoButton.Margin = new Padding(3, 4, 3, 4);
             ShowInfoButton.Name = "ShowInfoButton";
-            ShowInfoButton.Size = new Size(120, 36);
+            ShowInfoButton.Size = new Size(137, 48);
             ShowInfoButton.TabIndex = 15;
             ShowInfoButton.Text = "Show Info";
             ShowInfoButton.UseVisualStyleBackColor = true;
@@ -200,9 +195,9 @@
             label5.AutoSize = true;
             label5.BackColor = Color.FromArgb(192, 192, 255);
             label5.Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            label5.Location = new Point(61, 137);
+            label5.Location = new Point(70, 183);
             label5.Name = "label5";
-            label5.Size = new Size(132, 19);
+            label5.Size = new Size(172, 22);
             label5.TabIndex = 16;
             label5.Text = "Select your Birthday";
             // 
@@ -223,31 +218,35 @@
             panel1.Controls.Add(SaveButton);
             panel1.Controls.Add(EditButton);
             panel1.Controls.Add(closeButton);
-            panel1.Location = new Point(121, 34);
+            panel1.Location = new Point(138, 45);
+            panel1.Margin = new Padding(3, 4, 3, 4);
             panel1.Name = "panel1";
-            panel1.Size = new Size(602, 350);
+            panel1.Size = new Size(688, 511);
             panel1.TabIndex = 17;
             panel1.Paint += panel1_Paint;
             // 
             // textBox6
             // 
-            textBox6.Location = new Point(85, 67);
+            textBox6.Location = new Point(97, 89);
+            textBox6.Margin = new Padding(3, 4, 3, 4);
             textBox6.Name = "textBox6";
-            textBox6.Size = new Size(89, 23);
+            textBox6.Size = new Size(101, 27);
             textBox6.TabIndex = 13;
             // 
             // textBox5
             // 
-            textBox5.Location = new Point(85, 35);
+            textBox5.Location = new Point(97, 47);
+            textBox5.Margin = new Padding(3, 4, 3, 4);
             textBox5.Name = "textBox5";
-            textBox5.Size = new Size(89, 23);
+            textBox5.Size = new Size(101, 27);
             textBox5.TabIndex = 12;
             // 
             // textBox4
             // 
-            textBox4.Location = new Point(85, 5);
+            textBox4.Location = new Point(97, 7);
+            textBox4.Margin = new Padding(3, 4, 3, 4);
             textBox4.Name = "textBox4";
-            textBox4.Size = new Size(89, 23);
+            textBox4.Size = new Size(101, 27);
             textBox4.TabIndex = 11;
             textBox4.TextChanged += textBox4_TextChanged;
             // 
@@ -255,33 +254,37 @@
             // 
             label7.AutoSize = true;
             label7.BackColor = Color.LightSteelBlue;
-            label7.Location = new Point(70, 143);
+            label7.Location = new Point(80, 191);
             label7.Name = "label7";
-            label7.Size = new Size(85, 15);
+            label7.Size = new Size(108, 20);
             label7.TabIndex = 10;
             label7.Text = "Select Birthday";
             // 
             // monthCalendar2
             // 
-            monthCalendar2.Location = new Point(9, 167);
+            monthCalendar2.Location = new Point(34, 220);
+            monthCalendar2.Margin = new Padding(10, 12, 10, 12);
             monthCalendar2.Name = "monthCalendar2";
             monthCalendar2.TabIndex = 9;
             // 
             // comboBoxGender
             // 
             comboBoxGender.FormattingEnabled = true;
-            comboBoxGender.Location = new Point(85, 96);
+            comboBoxGender.Items.AddRange(new object[] { "Male", "Female", "Other." });
+            comboBoxGender.Location = new Point(97, 128);
+            comboBoxGender.Margin = new Padding(3, 4, 3, 4);
             comboBoxGender.Name = "comboBoxGender";
-            comboBoxGender.Size = new Size(87, 23);
+            comboBoxGender.Size = new Size(99, 28);
             comboBoxGender.TabIndex = 8;
+            comboBoxGender.SelectedIndexChanged += comboBoxGender_SelectedIndexChanged;
             // 
             // genderLabel
             // 
             genderLabel.AutoSize = true;
             genderLabel.BackColor = Color.LightSteelBlue;
-            genderLabel.Location = new Point(20, 99);
+            genderLabel.Location = new Point(23, 132);
             genderLabel.Name = "genderLabel";
-            genderLabel.Size = new Size(45, 15);
+            genderLabel.Size = new Size(57, 20);
             genderLabel.TabIndex = 7;
             genderLabel.Text = "Gender";
             // 
@@ -289,9 +292,9 @@
             // 
             ageLabel.AutoSize = true;
             ageLabel.BackColor = Color.LightSteelBlue;
-            ageLabel.Location = new Point(20, 67);
+            ageLabel.Location = new Point(23, 89);
             ageLabel.Name = "ageLabel";
-            ageLabel.Size = new Size(31, 15);
+            ageLabel.Size = new Size(39, 20);
             ageLabel.TabIndex = 6;
             ageLabel.Text = "Age:";
             // 
@@ -299,9 +302,9 @@
             // 
             surnameLabel.AutoSize = true;
             surnameLabel.BackColor = Color.LightSteelBlue;
-            surnameLabel.Location = new Point(20, 38);
+            surnameLabel.Location = new Point(23, 51);
             surnameLabel.Name = "surnameLabel";
-            surnameLabel.Size = new Size(57, 15);
+            surnameLabel.Size = new Size(70, 20);
             surnameLabel.TabIndex = 5;
             surnameLabel.Text = "Surname:";
             surnameLabel.Click += label7_Click;
@@ -310,47 +313,53 @@
             // 
             label6.AutoSize = true;
             label6.BackColor = Color.LightSteelBlue;
-            label6.Location = new Point(20, 13);
+            label6.Location = new Point(23, 17);
             label6.Name = "label6";
-            label6.Size = new Size(42, 15);
+            label6.Size = new Size(52, 20);
             label6.TabIndex = 4;
             label6.Text = "Name:";
             // 
             // ProductsList
             // 
             ProductsList.FormattingEnabled = true;
-            ProductsList.ItemHeight = 15;
-            ProductsList.Location = new Point(250, 143);
+            ProductsList.ItemHeight = 20;
+            ProductsList.Location = new Point(264, 223);
+            ProductsList.Margin = new Padding(3, 4, 3, 4);
             ProductsList.Name = "ProductsList";
-            ProductsList.Size = new Size(346, 139);
+            ProductsList.Size = new Size(417, 204);
             ProductsList.TabIndex = 3;
             ProductsList.SelectedIndexChanged += ProductsList_SelectedIndexChanged;
             // 
             // SaveButton
             // 
-            SaveButton.Location = new Point(499, 294);
+            SaveButton.Location = new Point(571, 445);
+            SaveButton.Margin = new Padding(3, 4, 3, 4);
             SaveButton.Name = "SaveButton";
-            SaveButton.Size = new Size(84, 39);
+            SaveButton.Size = new Size(96, 52);
             SaveButton.TabIndex = 2;
             SaveButton.Text = "Save";
             SaveButton.UseVisualStyleBackColor = true;
+            SaveButton.Click += SaveButton_Click;
             // 
             // EditButton
             // 
-            EditButton.Location = new Point(514, 86);
+            EditButton.Location = new Point(573, 138);
+            EditButton.Margin = new Padding(3, 4, 3, 4);
             EditButton.Name = "EditButton";
-            EditButton.Size = new Size(82, 38);
+            EditButton.Size = new Size(94, 51);
             EditButton.TabIndex = 1;
             EditButton.Text = "Edit";
             EditButton.UseVisualStyleBackColor = true;
+            EditButton.Click += EditButton_Click;
             // 
             // closeButton
             // 
             closeButton.BackColor = Color.Crimson;
             closeButton.ForeColor = SystemColors.ControlLightLight;
-            closeButton.Location = new Point(569, 3);
+            closeButton.Location = new Point(650, 4);
+            closeButton.Margin = new Padding(3, 4, 3, 4);
             closeButton.Name = "closeButton";
-            closeButton.Size = new Size(27, 25);
+            closeButton.Size = new Size(31, 33);
             closeButton.TabIndex = 0;
             closeButton.Text = "X";
             closeButton.UseVisualStyleBackColor = false;
@@ -358,10 +367,10 @@
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.MidnightBlue;
-            ClientSize = new Size(743, 429);
+            ClientSize = new Size(849, 572);
             Controls.Add(panel1);
             Controls.Add(label5);
             Controls.Add(ShowInfoButton);
@@ -377,9 +386,9 @@
             Controls.Add(deleteButton);
             Controls.Add(button1);
             Controls.Add(listBox1);
-            Margin = new Padding(3, 2, 3, 2);
             Name = "Form1";
             Text = "Form1";
+            Load += Form1_Load;
             panel1.ResumeLayout(false);
             panel1.PerformLayout();
             ResumeLayout(false);
--- personsList/Form1.cs
+++ personsList/Form1.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Reflection;
 using System.Windows.Forms;
 
 namespace personsList
@@ -18,6 +20,9 @@ namespace personsList
 
                 Person p1 = new Person(textBox1.Text, textBox2.Text, Gender.SelectedItem.ToString(), Convert.ToInt32(textBox3.Text), monthCalendar1.SelectionStart); personsList.Add(p1);
                 listBox1.Items.Add(p1);
+                textBox1.Clear(); textBox2.Clear(); textBox3.Clear();
+                Gender.SelectedIndex = -1;
+                monthCalendar1.SetDate(DateTime.Now);
             }
             else
             {
@@ -70,14 +75,51 @@ namespace personsList
 
         private void closeButton_Click(object sender, EventArgs e)
         {
-            panel1.Hide();
+            panel1.Hide(); EditButton_Click(sender, e);
         }
 
         private void panel1_Paint(object sender, PaintEventArgs e)
         {
+            int index = listBox1.SelectedIndex;
+            if (index != -1 && !textBox5.Enabled)
+            {
 
+                textBox4_SetText();
+                textBox5_SetText();
+                textBox6_SetText();
+                genderComboBox_Set();
+                monthCalendar2_Set();
+
+            }
+            }
+
+        private void monthCalendar2_Set()
+        {
+            int index = listBox1.SelectedIndex;
+            if (index != -1)
+            {
+                monthCalendar2.SetDate(personsList[index].Birthday);
+            }
         }
-
+        private void genderComboBox_Set()
+        {
+            int index = listBox1.SelectedIndex;
+            if(index != -1)
+            {
+                switch (personsList[index].Gender)
+                {
+                    case "Male":
+                        comboBoxGender.SelectedIndex = 0;
+                        break;
+                    case "Female":
+                        comboBoxGender.SelectedIndex = 1;
+                        break;
+                    case "Other.":
+                        comboBoxGender.SelectedIndex = 2;
+                        break;
+                }
+            }
+        }
         private void ShowInfoButton_Click(object sender, EventArgs e)
         {
             panel1.Show();
@@ -85,8 +127,88 @@ namespace personsList
 
         private void textBox4_TextChanged(object sender, EventArgs e)
         {
+
+        }
+        private void textBox4_SetText()
+        {
+
             int ind = listBox1.SelectedIndex;
-            textBox4.Text = String.Format(personsList[ind].Name);
+            if (ind != -1)
+            {
+                
+                    textBox4.Text = personsList[ind].Name;
+                    textBox4.ForeColor = Color.Gray;
+                
+            }
+            textBox4.ForeColor = Color.Black;
+
+        }
+
+        private void textBox5_SetText()
+        {
+
+            int ind = listBox1.SelectedIndex;
+            if (ind != -1)
+            {
+                    textBox5.Text = personsList[ind].Surname;
+                    textBox5.ForeColor = Color.Gray;
+                }
+            textBox5.ForeColor = Color.Black;
+        }
+
+        private void textBox6_SetText()
+        {
+
+            int ind = listBox1.SelectedIndex;
+            if (ind != -1)
+            {
+                    textBox6.Text = personsList[ind].Age.ToString();
+                    textBox6.ForeColor = Color.Gray;
+                
+            }
+            textBox6.ForeColor = Color.Black;
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            panel1.Hide(); EditButton_Click(sender, e);
+
+        }
+
+        private void comboBoxGender_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void EditButton_Click(object sender, EventArgs e)
+        {
+            textBox4.Enabled = !textBox4.Enabled;
+            textBox5.Enabled = !textBox5.Enabled;
+            textBox6.Enabled = !textBox6.Enabled;
+            comboBoxGender.Enabled = !comboBoxGender.Enabled;
+            monthCalendar2.Enabled = !monthCalendar2.Enabled;
+
+        }
+
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+            int selectedIndex = listBox1.SelectedIndex;
+
+            if (selectedIndex >= 0 && selectedIndex < listBox1.Items.Count)
+            {
+                listBox1.Items.RemoveAt(selectedIndex);
+                personsList.RemoveAt(selectedIndex);
+                Person p2 = new Person(textBox4.Text, textBox5.Text, comboBoxGender.SelectedItem.ToString(), Convert.ToInt32(textBox6.Text), monthCalendar2.SelectionStart);
+                personsList.Insert(selectedIndex, p2);
+                listBox1.Items.Insert(selectedIndex, p2);
+            }
+            else
+            {
+                MessageBox.Show("No item selected or invalid selection.");
+            }
+            panel1.Hide();
+            EditButton_Click(sender,e);
         }
     }
 }
